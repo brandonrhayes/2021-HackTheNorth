9f93ab7e92b17b6bc5c88757e98f03c0631ec95b
hypnoticpenguin <chankevin234@gmail.com>
2021-01-16 12:12:45 -0500

    created connection to cockroachcloud cluster's database with nodeJS

---
+++ hackthenorth/server/db/connectDB.js
@@ -0,0 +1,72 @@
+var async = require("async");
+var fs = require("fs");
+var pg = require("pg");
+
+// Connect to the "modern-puma-267.defaultdb" database.
+var config = {
+  user: "kevin",
+  password: "password1234567890",
+  host: "free-tier.gcp-us-central1.cockroachlabs.cloud",
+  database: "modern-puma-267.defaultdb",
+  port: 26257,
+  ssl: {
+    ca: fs.readFileSync("certs/cc-ca.crt").toString(),
+  },
+};
+
+// Create a pool.
+var pool = new pg.Pool(config);
+
+pool.connect(function (err, client, done) {
+  // Close communication with the database and exit.
+  var finish = function () {
+    done();
+    process.exit();
+  };
+
+  if (err) {
+    console.error("could not connect to cockroachdb", err);
+    finish();
+  }
+  async.waterfall(
+    [
+      function (next) {
+        // Create the 'sponsors' table.
+        client.query(
+          "CREATE TABLE IF NOT EXISTS sponsors (id INT PRIMARY KEY, balance INT);",
+          next
+        );
+      },
+      function (results, next) {
+        // Insert two rows into the 'accounts' table.
+        client.query(
+          "INSERT INTO sponsors (id, balance) VALUES (1, 1000), (2, 250);",
+          next
+        );
+      },
+      function (results, next) {
+        // Print out account balances.
+        client.query("SELECT id, balance FROM sponsors;", next);
+      },
+    ],
+    function (err, results) {
+      if (err) {
+        console.error(
+          "Error inserting into and selecting from sponsors: ",
+          err
+        );
+        finish();
+      }
+
+      console.log("Initial balances:");
+      results.rows.forEach(function (row) {
+        console.log(row);
+      });
+
+      finish();
+    }
+  );
+});
+// Your code goes here.
+// For more information, see the 'node-postgres' docs:
+// https://node-postgres.com
