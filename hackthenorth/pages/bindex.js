eb93939f6d42099d2bdc53f5471e91ee8f864f4a
Brandon Hayes <47832068+brandonrhayes@users.noreply.github.com>
2021-01-16 11:15:17 -0500

    Updated the UI. 2 pages. Main Page & Featured Foods.

--- hackthenorth/pages/bindex.js
+++ hackthenorth/pages/bindex.js
@@ -1,37 +1,29 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import layout from '../styles/Layout.module.css'
-import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>ShareFood</title>
-        <link rel="icon" href="/bake.png" />
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to ShareFood!
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <div className={layout.container}>
-            <p className={styles.description}>
-            We're an online community commited to helping individuals connect
-            to find and share food.  We know that preparing meals during the school year
-            can be challenging, and so do our partner familes.  Don't let stress and the 
-            pandemic let you feel insecure about the food you're eating. Connect with a
-            meal provider today to get quick access to nutritious meals for free!
-            </p>
-        </div>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
 
         <div className={styles.grid}>
-          <Link href="/posts/featured-posts">
-            <a className={styles.card}>
-            <h3>Featured Meals &rarr;</h3>
-            <p>Checkout today's featured meals by our favourite contributors.</p>
-            </a>
-          </Link>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h3>Learn &rarr;</h3>
@@ -67,10 +59,6 @@ export default function Home() {
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
-        <p>{' '}</p>
-        
-        - Website icon made by <a href="https://www.flaticon.com/authors/ultimatearm" title="ultimatearm">ultimatearm</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-
       </footer>
     </div>
   )
