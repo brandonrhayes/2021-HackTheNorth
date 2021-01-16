4fb2f3e87741b8074377d105fa7814ea874e05d2
Brandon Hayes <47832068+brandonrhayes@users.noreply.github.com>
2021-01-16 12:12:26 -0500

    Added Nav to Top Posts, Removed extra index

--- hackthenorth/pages/posts/featured-posts.js
+++ hackthenorth/pages/posts/featured-posts.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import Toolbar from '@material-ui/core/Toolbar'
-import Paper from '@material-ui/core/Paper'
-import Typography from '@material-ui/core/Typography'
-import Grid from '@material-ui/core/Grid'
-import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea'
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
-import Hidden from '@material-ui/core/Hidden'
-import Link from '@material-ui/core/Link'
-import Container from '@material-ui/core/Container'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Hidden from '@material-ui/core/Hidden';
+import Link from '@material-ui/core/Link';
+import Container from '@material-ui/core/Container';
+import Nav from '../../components/navbar';
 
 const useStyles = makeStyles(theme => ({
     toolbar: {
@@ -55,7 +56,7 @@ const useStyles = makeStyles(theme => ({
       cardMedia: {
         width: 160
       }
-}))
+}));
 
 const featuredPosts = [
   {
@@ -70,13 +71,14 @@ const featuredPosts = [
     description:
       'A delicious homemade meal.'
   }
-]
+];
 
 const FeaturedPosts = () => {
   const classes = useStyles()
 
   return (
     <>
+      <Nav> </Nav>
       <CssBaseline />
       <Container maxWidth="lg">
         <Toolbar className={classes.toolbar}>
