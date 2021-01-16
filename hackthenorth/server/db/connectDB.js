b834edfdee0d15240a09c5cfa3836933982779d2
hypnoticpenguin <chankevin234@gmail.com>
2021-01-16 15:58:49 -0500

    updates table with insert function

--- hackthenorth/server/db/connectDB.js
+++ hackthenorth/server/db/connectDB.js
@@ -14,59 +14,67 @@ var config = {
   },
 };
 
+//List of all the functions
+//----------------------------------
+//- InsertSponsor
+//- update sponsor data
+//- delete sponsor data
+//- insert student
+//- update student
+//- delete student
+//- update sponsor click count (add field/count to both student and sponsor count)
+//- toggle show sponsor data (boolean)
+//-
+
 // Create a pool.
 var pool = new pg.Pool(config);
 
-pool.connect(function (err, client, done) {
-  // Close communication with the database and exit.
-  var finish = function () {
-    done();
-    process.exit();
-  };
-  //checks for errors
-  if (err) {
-    console.error("could not connect to cockroachdb", err);
-    finish();
-  }
-  async.waterfall(
-    [
-      function (next) {
-        // Create the 'sponsors' table.
-        client.query(
-          "CREATE TABLE IF NOT EXISTS sponsors (id INT PRIMARY KEY, fname varchar, lname varchar, org varchar);",
-          next
-        );
-      },
-      function (results, next) {
-        // Insert 3 rows into the 'sponsors' table.
-        client.query(
-          "INSERT INTO sponsors (id, fname, lname, org) VALUES (12, 'Edmund', 'Lui', 'HTN'), (22, 'Adams', 'Liu', 'HTN'), (33, 'Brandon', 'Hayes', 'HTN');",
-          next
-        );
-      },
-      function (results, next) {
-        // Print out sponsors list.
-        client.query("SELECT * FROM sponsors;", next);
-      },
-    ],
-    function (err, results) {
-      if (err) {
-        console.error(
-          "Error inserting into and selecting from sponsors: ",
-          err
-        );
-        finish();
-      }
-
-      console.log("Initial rows:");
-      results.rows.forEach(function (row) {
-        console.log(row);
-      });
-
+function createSponsor() {
+  pool.connect(function (err, client, done) {
+    // Close communication with the database and exit.
+    var finish = function () {
+      done();
+      process.exit();
+    };
+    //checks for errors
+    if (err) {
+      console.error("could not connect to cockroachdb", err);
       finish();
     }
-  );
-});
-// Your code goes here.
-// For more information, see the 'node-postgres' docs:
-// https://node-postgres.com
+    async.waterfall(
+      [
+        // function (next) {
+        //   // Create the 'sponsors' table.
+        //   client.query(
+        //     "CREATE TABLE IF NOT EXISTS sponsors (id INT PRIMARY KEY, fname varchar, lname varchar, org varchar);",
+        //     next
+        //   );
+        // },
+        function (results, next) {
+          // Insert 3 rows into the 'sponsors' table.
+          client.query(
+            "INSERT INTO sponsors (id, fname, lname, org) VALUES (51 , 'Edmund', 'Lui', 'HTN'), (52, 'Adams', 'Liu', 'HTN'), (81, 'Brandon', 'Hayes', 'HTN');"
+          );
+          console.log("YOOYOYO");
+        },
+      ],
+      function (err, results) {
+        if (err) {
+          console.error(
+            "Error inserting into and selecting from sponsors: ",
+            err
+          );
+          console.log("OYOYOYOY");
+          finish();
+        }
+        finish();
+      }
+    );
+  });
+  // Your code goes here.
+  // For more information, see the 'node-postgres' docs:
+  // https://node-postgres.com
+}
+
+var result = createSponsor();
+console.log(result);
