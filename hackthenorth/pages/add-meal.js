2eeb5261027ce8627bef9890fff90b286452951b
adams <adamsliu97@gmail.com>
2021-01-16 23:24:32 -0500

    made small changes

---
+++ hackthenorth/pages/add-meal.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import { FormControl } from "@material-ui/core";
+
+const AddMeal = () => {
+  return (
+    <FormControl>
+      <FormHelperText id="my-helper-text">
+        We'll never share your email.
+      </FormHelperText>
+    </FormControl>
+  );
+};
+
+export default AddMeal;
